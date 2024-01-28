82dfe00379cc113f229292dec52534b70a6c6705
Dmitry <minskiyda@gmail.com>
2024-01-28 11:55:05 +0000

    define col names as consts to avoid misspelling

---
+++ utils/col_names.py
@@ -0,0 +1,19 @@
+RATE = "rate"
+DISTANCE = "valid_miles"
+TRANSPORT_TYPE = "transport_type"
+TRANSPORT_TYPES = [
+    "transport_type_GJROY",
+    "transport_type_KFEGT",
+    "transport_type_MKPFX",
+]
+WEIGHT = "weight"
+PICKUP_DATE = "pickup_date"
+PICKUP_DATE_DAY = "pickup_date_day"
+PICKUP_DATE_WEEK_DAY = "pickup_date_week_day"
+PICKUP_DATE_YEAR = "pickup_date_year"
+PICKUP_DATE_MONTH = "pickup_date_month"
+PICKUP_DATE_HOUR = "pickup_date_hour"
+ORIGIN = "origin_kma"
+ORIGIN_ENCODED = "origin_kma_encoded"
+DESTINATION = "destination_kma"
+DESTINATION_ENCODED = "destination_kma_encoded"
