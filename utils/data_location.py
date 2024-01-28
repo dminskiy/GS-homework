adb1c79f0f70b4aea64d90e8573f0a8901ca3e05
Dmitry <minskiyda@gmail.com>
2024-01-28 11:55:27 +0000

    Define data locations globally

---
+++ utils/data_location.py
@@ -0,0 +1,15 @@
+import os
+
+DATA_DIR = "dataset"
+DATA_DIR_TR_V1 = os.path.join(DATA_DIR, "transformed_v1")
+DATA_DIR_TR_V2 = os.path.join(DATA_DIR, "transformed_v2")
+
+VISIALISATIONS_DIR = "visualisations"
+MODELS_DIR = "trained_models"
+FINAL_PREDICTIONS_DIR = "final_predictions"
+
+TRAINING_SET_FILE = os.path.join(DATA_DIR, "train.csv")
+VALIDATION_SET_FILE = os.path.join(DATA_DIR, "validation.csv")
+TEST_SET_FILE = os.path.join(DATA_DIR, "test.csv")
+
+DATA_FILES = [TRAINING_SET_FILE, TEST_SET_FILE, VALIDATION_SET_FILE]
